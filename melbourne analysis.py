82a14ee7044bf6bc391eb6b3d1639c7ff5c3271b
Faizal Shebli <87697272+faizalshebli@users.noreply.github.com>
2021-09-11 07:22:18 +0800

    Add files via upload

---
+++ melbourne analysis.py
@@ -0,0 +1,31 @@
+#import libraries
+import pandas as pd
+
+#read data source file
+melbourne_data = pd.read_csv('/Users/faizalshebli/Documents/#FS2021/FS transformation/FS5T plan/FS learn ML via Kaggle/Data/melb_data.csv')
+
+#data set exploratory
+melbourne_data.head()
+
+#remove missing value 'na'
+melbourne_data = melbourne_data.dropna(axis=0)
+
+#selecting the predeition target
+y = melbourne_data.Price
+
+#selecting the features
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+
+X=melbourne_data[melbourne_features]
+
+#building my model
+
+from sklearn.tree import DecisionTreeRegressor
+#Define model. Specify a number for random_state to ensure same results each run
+
+melbourne_model = DecisionTreeRegressor(random_state=1)
+
+#Fit model
+melbourne_model.fit(X, y)
+
+DecisionTreeRegressor(random_state=1)
